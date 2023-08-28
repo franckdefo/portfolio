7aeb0074a7ca89874c5cdf71715b9f051658e67a
franckdefo <defofranck30@gmail.com>
2023-08-28 16:08:03 +0100

    mise a jour responsivitté et cv

--- script.js
+++ script.js
@@ -41,44 +41,44 @@ ScrollReveal({
   delay:400
 });
 
-ScrollReveal().reveal('.header_profil',{delay:500,origin:'button'});
-ScrollReveal().reveal('.header_title',{delay:500,origin:'left'});
+ScrollReveal().reveal('.header_profil',{delay:350,origin:'button'});
+ScrollReveal().reveal('.header_title',{delay:350,origin:'left'});
 
 /*partenaire*/
-ScrollReveal().reveal('.partenaire_container',{delay:500,origin:'top'});
+ScrollReveal().reveal('.partenaire_container',{delay:350,origin:'top'});
 
 /*services*/
-ScrollReveal().reveal('.titr',{delay:500,origin:'top'});
-ScrollReveal().reveal('.service1',{delay:400,origin:'top'});
-ScrollReveal().reveal('.service2',{delay:700,origin:'top'});
-ScrollReveal().reveal('.service3',{delay:800,origin:'top'});
+ScrollReveal().reveal('.titr',{delay:350,origin:'top'});
+ScrollReveal().reveal('.service1',{delay:300,origin:'top'});
+ScrollReveal().reveal('.service2',{delay:550,origin:'top'});
+ScrollReveal().reveal('.service3',{delay:700,origin:'top'});
 
 /*projets*/
-ScrollReveal().reveal('.title_projet',{delay:500,origin:'top'});
-ScrollReveal().reveal('.card_projet',{delay:400,origin:'top'});
-ScrollReveal().reveal('.card_projet',{delay:500,origin:'top'});
-ScrollReveal().reveal('.card_projet',{delay:500,origin:'top'});
-ScrollReveal().reveal('.card_projet',{delay:500,origin:'top'});
+ScrollReveal().reveal('.title_projet',{delay:350,origin:'top'});
+ScrollReveal().reveal('.card_projet',{delay:300,origin:'top'});
+ScrollReveal().reveal('.card_projet',{delay:350,origin:'top'});
+ScrollReveal().reveal('.card_projet',{delay:350,origin:'top'});
+ScrollReveal().reveal('.card_projet',{delay:350,origin:'top'});
 
 /*blog*/
-ScrollReveal().reveal('.blog',{delay:400,origin:'top'});
-ScrollReveal().reveal('.blog',{delay:500,origin:'top'});
-ScrollReveal().reveal('.reference',{delay:600,origin:'top'});
+ScrollReveal().reveal('.blog',{delay:300,origin:'top'});
+ScrollReveal().reveal('.blog',{delay:350,origin:'top'});
+ScrollReveal().reveal('.reference',{delay:500,origin:'top'});
 
 /*profil*/
-ScrollReveal().reveal('.img_profil',{delay:400,origin:'left'});
-ScrollReveal().reveal('.description',{delay:400,origin:'button'});
+ScrollReveal().reveal('.img_profil',{delay:300,origin:'left'});
+ScrollReveal().reveal('.description',{delay:300,origin:'button'});
 
 /*experience*/
-ScrollReveal().reveal('.formation',{delay:400,origin:'left'});
-ScrollReveal().reveal('.experience',{delay:400,origin:'button'});
+ScrollReveal().reveal('.formation',{delay:300,origin:'left'});
+ScrollReveal().reveal('.experience',{delay:300,origin:'button'});
 
 /*temoignage*/
-ScrollReveal().reveal('.mySwiper',{delay:500,origin:'top'});
+ScrollReveal().reveal('.mySwiper',{delay:350,origin:'top'});
 
 /*footer*/
-ScrollReveal().reveal('.contact_container',{delay:500,origin:'top'});
+ScrollReveal().reveal('.contact_container',{delay:350,origin:'top'});
 
 /*A propos */
-ScrollReveal().reveal('.name',{delay:500,origin:'button'});
-ScrollReveal().reveal('.moi',{delay:500,origin:'left'});
+ScrollReveal().reveal('.name',{delay:350,origin:'button'});
+ScrollReveal().reveal('.moi',{delay:350,origin:'left'});
